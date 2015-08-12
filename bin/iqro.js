bbafe46ea0a8e21a3c32c4fa634c680001327c8a
Dhi Aurrahman <diorahman@rockybars.com>
2015-08-12 18:55:54 +0700

    tidy up

--- bin/iqro.js
+++ bin/iqro.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 var Player = require('player');
 var ROOT = 'http://download.quranicaudio.com/quran/tawfeeq_bin_saeed-as-sawaaigh';
-function zeroPad(num, places) {
-  var zero = places - num.toString().length + 1;
-  return Array(+(zero > 0 && zero)).join("0") + num;
-}
 var argv = require('minimist')(process.argv.slice(2));
 var surahs = argv._.filter(function(surah) {
   if (parseInt(surah) != NaN && surah <= 114 && surah >= 1)
@@ -13,12 +9,16 @@ var surahs = argv._.filter(function(surah) {
   return ROOT + '/' + zeroPad(surah, 3) + '.mp3';
 });
 var player = new Player(surahs);
-player.on('playing', function(item){
+player.on('playing', function(item) {
   console.log('-> ', item._name.split('.')[0]);
-})
-player.on('error', function(){
+});
+player.on('error', function() {
   // not yet handled
 });
 player.play();
 console.log('Downloading file...');
 
+function zeroPad(num, places) {
+  var zero = places - num.toString().length + 1;
+  return Array(+(zero > 0 && zero)).join("0") + num;
+}
